eb5ff58a72ef1e9d2c141bdbff761c5923b4bf9f
Regina Wong <reginawongmm@gmail.com>
2021-07-08 22:58:04 -0400

    SH-5 Setup gatsby app and nodejs plugin

---
+++ ui/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "syntax-highlight",
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-gatsby-cloud"],
+};

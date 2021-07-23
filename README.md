93ce037091c681b44c80d1571355821b48cc73ee
YutianLin <me@yutianlin.com>
2021-07-23 10:38:28 -0700

    WIP

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # syntax-highlight
 ## To Start the Project
-1. syntax-highlight/server\$ `yarn start`
+1. syntax-highlight/server\$ `yarn start_elastic_search`
+1. syntax-highlight/server\$ `yarn start_server`
 
 ## To Clean the Project
 1. syntax-highlight/server\$ `yarn clean`
 
-## To Setup Project
-1. Install Node.js, npm, yarn, homebrew
+## To Setup Project on Mac
+1. `brew install yarn`
+1. `brew tap elastic/tap`
+1. `brew install elastic/tap/elasticsearch-full`
+1. `brew install node`
+1. `brew install typescript`
--- server/src/ElasticSearchService.ts
+++ server/src/ElasticSearchService.ts
@@ -10,7 +10,60 @@ export default class ElasticSearchService {
         })
     }
 
-    public async ping() {
+    public ping() {
         return this.elasticClient.ping({requestTimeout: 30000});
+    }
+
+    public initIndex(indexName: string) {
+        return this.elasticClient.indices.create({
+            index: indexName
+        });
+    }
+
+    public indexExists(indexName: string) {
+        return this.elasticClient.indices.exists({
+            index: indexName
+        });
+    }
+
+    public initMapping(indexName: string, docType: string, payload: JSON) {
+        return this.elasticClient.indices.putMapping({
+            index: indexName,
+            type: docType,
+            body: payload
+        });
+    }
+
+    public addDocument(indexName: string, docType: string, payload: JSON) {
+        return this.elasticClient.index({
+            index: indexName,
+            type: docType,
+            body: payload
+        });
+    }
+
+    public updateDocument(indexName: string, id: string, docType: string, payload: JSON) {
+        return this.elasticClient.update({
+            index: indexName,
+            type: docType,
+            id: id,
+            body: payload
+        });
+    }
+
+    public search(indexName: string, docType: string, payload: JSON) {
+        this.elasticClient.search({
+            index: indexName,
+            type: docType,
+            body: payload
+        });
+    }
+
+    public deleteDocument(indexName: string, id: string, docType: string) {
+        this.elasticClient.delete({
+            index: indexName,
+            type: docType,
+            id: id
+        });
     }
 }
